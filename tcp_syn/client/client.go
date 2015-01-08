8a4b7c1a9614bc35d3d7802b034660b280a4aa15
subhash <subhash_singh@criterionnetworks.com>
2015-01-09 02:57:56 +0530

    reduced the connection limit to 1000 from 10000. fixed the waitgroup deadlock.

--- tcp_syn/client/client.go
+++ tcp_syn/client/client.go
@@ -3,31 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
+	// "os"
 	"sync"
+	"time"
 )
 
-func connect(address string, wg sync.WaitGroup) {
+func connect(address string, id int, wg *sync.WaitGroup) {
 	conn, err := net.Dial("tcp", address)
 	if err != nil {
 		fmt.Println(err.Error())
-		os.Exit(-1)
+		// os.Exit(-1)
 	}
 	defer conn.Close()
 
-	fmt.Println("connection is established")
-	//wg.Done()
+	fmt.Println("connection is established : ", id)
+	defer wg.Done()
 }
 
 func main() {
 	var wg sync.WaitGroup
-	for i := 0; i < 10000; i++ {
-		// wg.Add(1)
-		fmt.Printf("connection %d", i)
-		fmt.Println()
-		go connect(":9100", wg)
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		time.Sleep(time.Millisecond)
+		fmt.Println("connection", i)
+		go connect(":9100", i, &wg)
 	}
 
 	wg.Wait()
-	fmt.Print("all the connection served")
 }
