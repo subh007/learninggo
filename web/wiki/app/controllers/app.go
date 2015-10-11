ecb17443b81617b0d0d35231d66741291d3ac263
subhash <subh.singh007@gmail.com>
2015-10-11 22:31:16 +0530

    register and login page done.

--- web/wiki/app/controllers/app.go
+++ web/wiki/app/controllers/app.go
@@ -1,6 +1,7 @@
 package controllers
 
 import "github.com/revel/revel"
+import "fmt"
 
 type App struct {
 	*revel.Controller
@@ -8,4 +9,17 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) Login(user, pass string) revel.Result {
+	fmt.Println("username :" + user)
+	return c.Result
+}
+
+func (c App) RegisterPage() revel.Result{
+	return c.Render()
+}
+
+func(c App) Register(user, pass string) revel.Result{
+	return c.Result
 }
